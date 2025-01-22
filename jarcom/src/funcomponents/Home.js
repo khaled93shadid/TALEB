d3402bdd23795e336aa4c620b6c5f043312e9b7c
khaled93shadid <khaled_shadid@yahoo.com>
2025-01-22 08:13:08 +0300

    main page

--- jarcom/src/funcomponents/Home.js
+++ jarcom/src/funcomponents/Home.js
@@ -1,5 +1,6 @@
 import react from 'react';
 import { useState } from 'react';
+import '../stylesheet/Home.css';
 
 export default function Home  ()
 {
@@ -22,11 +23,28 @@ const increment =()=>
     }    
 return(
 <>
-<h1>{msg}</h1>
-<h1>counter:{count}</h1>
-<button onClick={increment}>+</button>
-<button onClick={decrement}>-</button>
-<button onClick={editmsg}>EDIT</button>
+<div id='imgdiv'>
+<br/>       
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+</div>
+
+<div id='divbody'>
+
+<div id='area1'><h1>share your thoughts&Ideas </h1></div>
+<div id='area2'><img id='imgarea1' src='https://cdn.pixabay.com/photo/2016/03/30/02/21/idea-1289871_640.jpg' /></div>
+<div id='area3'><h1>Earn money &give support</h1></div>
+<div id='area4'><img id='imgarea2' src='https://st3.depositphotos.com/12060448/14627/i/450/depositphotos_146277809-stock-photo-selective-focus-of-coins-and.jpg' /></div>
+
+</div>
 </>
 )
 
