80b5e43f75a2b8d6855d5b2d65f48f6346e80766
khaled93shadid <khaled_shadid@yahoo.com>
2025-02-04 20:49:47 +0300

    profile

--- jarcom/src/funcomponents/profile.js
+++ jarcom/src/funcomponents/profile.js
@@ -13,8 +13,20 @@ import TextField from '@mui/material/TextField';
 import { styled } from '@mui/material/styles';
 import CardActions from '@mui/material/CardActions';
 import BuildIcon from '@mui/icons-material/Build';
+import Modal from '@mui/material/Modal';
 
 
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
 
 
 export default function Profile(){
@@ -23,7 +35,42 @@ export default function Profile(){
         backgroundColor: theme.palette.background.paper,
         padding: theme.spacing(1),
       }));
-      
+
+      const [open, setOpen] = useState(false);
+      const handleOpen = () => setOpen(true);
+      const handleClose = () => setOpen(false);
+      const [name,setname]=useState('khaled shadid');
+      const [state,setstate]=useState('Im khaled shadid 31 years old ,Im customerService Advisor'); 
+      const[imagestate,setimagestate]=useState('https://www.skynewsarabia.com/images/v1/2023/12/10/1676794/1200/630/1-1676794.JPG');
+      const[coverstate,setcoverstate]=useState('https://wallpapercave.com/wp/wp9607777.jpg');
+      const editprofile=()=>{
+             const nametoedit=name;
+             setname(nametoedit)
+             const biotoedit=state;
+             setstate(biotoedit)
+             const imagetoedit = imagestate;
+             setimagestate(imagetoedit);
+             const covertoedit = coverstate;
+             setcoverstate(covertoedit);
+             alert('update profile successful')
+
+      }
+      const close=()=>{
+        handleClose();
+      }
+//share
+const[sharedata,setsharedata]=useState([
+  {txt:'Im enjoy my coffiee while working',image:'https://th.bing.com/th/id/OIP.018ajk3MILmEsi_64slCOQHaE8?rs=1&pid=ImgDetMain'},
+  {txt:'my breakfast for today',image:'https://th.bing.com/th/id/R.744c84bea7c61fad933497e23ab2f2da?rik=XUcQvbvPOM7yLQ&riu=http%3a%2f%2fcdn.taste.com.au%2fimages%2frecipes%2fsfi%2f2005%2f07%2f1018.jpg&ehk=45WmqnZucECJnQ4wEgA1lZ43QP1MLSldWj%2fqD%2bs%2fGY0%3d&risl=&pid=ImgRaw&r=0'},
+  {txt:'studying for the exam next week',image:'https://th.bing.com/th/id/OIP.n3sAXxDyR0gM6xT1ZwmXdQHaE8?rs=1&pid=ImgDetMain'}
+]);
+const[sharetxt,setsharetxt]=useState(null);
+const[shareimg,setshareimg]=useState(null);
+const deletepost=(index)=>{
+    const afterdelete= sharedata.filter((_,i)=>i !== index);
+    setsharedata(afterdelete);
+  }
+
 
 
 return(
@@ -36,16 +83,16 @@ return(
         <CardMedia
           component="img"
           height="140"
-          image="https://www.skynewsarabia.com/images/v1/2023/12/10/1676794/1200/630/1-1676794.JPG"
+          image={imagestate}
           alt="green iguana"
           id='img1'
         />
         <CardContent>
           <Typography gutterBottom variant="h5" component="div">
-            (khaled shadid)
+            {name}
           </Typography>
           <Typography variant="body2" sx={{ color: 'black' }}>
-            Im khaled shadid 31 years old ,Im customerService Advisor ,I study computer science at yurmok university 
+            {state} 
           </Typography>
         </CardContent>
       </CardActionArea>
@@ -55,7 +102,7 @@ return(
 
     <div id='area2'>
         
-      <img id='img55' src='https://wallpapercave.com/wp/wp9607777.jpg' />    
+      <img id='img55' src={coverstate} />    
        
     </div>
 
@@ -65,11 +112,61 @@ return(
     <br/>
     <br/>
     <br/>
+    <div>
+      <Button onClick={handleOpen} id='b1'>Update profile</Button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+        
+      >
+        <Box sx={style} id='modal'>
+          <Typography class='modaltxt' id="modal-modal-title" variant="h6" component="h2">
+            Update Profile:
+          </Typography>
+          <form class='modalform' onSubmit={(Event)=>{
+            Event.preventDefault();
+            /*
+            const updatedname = [name];
+            setname(updatedname);
+            const updatedstate=[state];
+            setstate(updatedstate);
+            const updatedimage=[imagestate];
+            setimagestate(updatedimage)
+            */
+          
+          }}>
+            <label > <h4 class='labelh4'>name:</h4> </label>
+            <input class='label' type='text' placeholder='name'  onChange={(Event)=>{setname(Event.target.value)}}/>
+            <br/>
+            <label > <h4 class='labelh4'>Personal Picture:</h4> </label>
+            <input class='label' type='text' placeholder='personal pic' onChange={(Event)=>{setimagestate(Event.target.value)}}/>
+            <br/>
+            <label > <h4 class='labelh4'>Cover Picture:</h4> </label>
+            <input class='label' type='text' placeholder='cover pic' onChange={(Event)=>{setcoverstate(Event.target.value)}}/>
+            <br/>
+            <label > <h4>Bio:</h4> </label>
+            <TextField
+          id="outlined-multiline-static"
+          label="write here bio"
+          multiline
+          rows={6}
+          defaultValue=""
+          onChange={(Event)=>{setstate(Event.target.value)}} />
+            <br/>
+            <br/>
+            <br/>
+            
+            <Button onClick={editprofile} variant="contained" disableElevation id='b5' type='submit'> update </Button>
+            <Button onClick={close} variant="contained" disableElevation id='b4' > close </Button>
+          </form>
+          
+        </Box>
+      </Modal>
+    </div>
     
-    <Button variant="contained" disableElevation id='b1'>
-      update profile  
-    </Button>
-    
+        
    
     </div>
 </div>
@@ -82,97 +179,83 @@ return(
 <div id='bodyprofile'>
     <div id='share'>
         <h1 id='h1share'>what do you think,share something... </h1>
-        <Box
-      component="form"
+        
+      <form onSubmit={(Event)=>{Event.preventDefault();
+      setsharedata([...sharedata,{txt:sharetxt,image:shareimg}])
+      setsharetxt('');
+      setshareimg('');
+        
+      }}>
+        
+      <Box
+      
       sx={{ '& .MuiTextField-root': { m: 1, width: '50ch' } }}
-      noValidate
+      
       autoComplete="off"
       id='box'
     >
-      <div>
+      <TextField
+          id="outlined-multiline-static"
+          label="image url"
+          multiline
+          rows={3}
+          value={shareimg}
+          onChange={(Event)=>{setshareimg(Event.target.value)}}
+        />
       <TextField
           id="outlined-multiline-static"
           label="write here"
           multiline
-          rows={6}
-          defaultValue=""
+          rows={3}
+          value={sharetxt}
+          onChange={(Event)=>{setsharetxt(Event.target.value)}}
         />
-        
-        </div>
-        <Button variant="contained" disableElevation id='b2'>
-      share
+        <br />
+        <br />
+        <Button  type='submit' variant="contained" disableElevation id='b2'>
+      share post
     </Button>
-        </Box>
+    </Box>
+        </form>
+        
+        
         
     </div>
 
 </div>
 
-<div  className='cardprofile'>
 
-<Card sx={{ maxWidth: 345 }}>
+
+  {sharedata.map((item,index)=>
+    <div  className='cardprofile' key={index}>  
+
+    <div>
+  <Card sx={{ maxWidth: 345 }} >
       <CardActionArea >
         <CardMedia
           component="img"
           height="140"
-          image="https://th.bing.com/th/id/OIP.018ajk3MILmEsi_64slCOQHaE8?rs=1&pid=ImgDetMain"
-          alt="picture"
-          
-          
-        />
-        
-      </CardActionArea>
+          image={item.image}
+          alt="picture" />
+       </CardActionArea>
       <CardActions>
-      <Button variant="contained" disableElevation className='b3'>
-      delete
-    </Button>      
+      <Button variant="contained" disableElevation id='b3' onClick={()=>{deletepost(index)}}>
+      delete post
+    </Button>            
     </CardActions>
+    
     </Card>
-    <h3 className='h22'>text here</h3>
-</div>
-<div  className='cardprofile'>
+    </div>
+    <div id='divsharetxt'>
+    <h3 className='h22'> {item.txt} </h3>
+    </div>
 
-<Card sx={{ maxWidth: 345 }} >
-      <CardActionArea >
-        <CardMedia
-          component="img"
-          height="140"
-          image="https://th.bing.com/th/id/OIP.Hu0hRphGtbw9LdRWftt9hgHaE8?rs=1&pid=ImgDetMain"
-          alt="picture"
-         
-          
-        />
-      </CardActionArea>
-      <CardActions>
-      <Button variant="contained" disableElevation className='b3'>
-      delete
-    </Button>
-      </CardActions>
-    </Card>
-    <h3 className='h22'>text here</h3>
-</div>
-<div className='cardprofile'>
+    </div>
+  
+  )}
 
-<Card sx={{ maxWidth: 345 }}>
-      <CardActionArea >
-        <CardMedia
-          component="img"
-          height="140"
-          image="https://th.bing.com/th/id/OIP.TeVPb13LkxwuEeOyy4783gHaEK?w=1920&h=1080&rs=1&pid=ImgDetMain"
-          alt="picture"
-          
-          
-        />
-        
-      </CardActionArea>
-      <CardActions>
-      <Button variant="contained" disableElevation className='b3'>
-      delete
-    </Button>
-      </CardActions>
-    </Card>
-    <h3 className='h22'>text here</h3>
-</div>
+
+
 <div id='imgdiv'>
 <br/>       
 <br/>
@@ -182,7 +265,6 @@ return(
 <br/>
 <br/>
 </div>
-
 </>
 )
 }
